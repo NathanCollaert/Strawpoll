bfef29f58c64e01c9cc54761242fea37fcbf6adc
NathanCollaert <25828721+NathanCollaert@users.noreply.github.com>
2020-01-04 22:57:14 +0100

    Initalizing data

--- StrawpollAPI/PollAPI/Data/DataInitializer.cs
+++ StrawpollAPI/PollAPI/Data/DataInitializer.cs
@@ -23,16 +23,16 @@ namespace PollAPI.Data
 
                 List<Strawpoll> strawpolls = new List<Strawpoll>()
                 {
-                    new Strawpoll("Does this question have a lot of answers?",DateTime.Now, new List<Answer>(){ new Answer("Yes",50),new Answer("No",8),  new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50), new Answer("Yes",50),}),
-                    new Strawpoll("How big am I?",DateTime.Now, new List<Answer>(){ new Answer("1m95",5),new Answer("1m92",8),new Answer("1m98",150),new Answer("1m96",500) }),
-                    new Strawpoll("How much do I weight?",DateTime.Now, new List<Answer>(){ new Answer("84",5),new Answer("87",8),new Answer("89",150),new Answer("85",500) }),
-                    new Strawpoll("What is my favorite sport?",DateTime.Now, new List<Answer>(){ new Answer("Voetbal",5),new Answer("Tennis",8),new Answer("Krachtbal",150),new Answer("Basketbal",500) }),
-                    new Strawpoll("Hoe noem ik?",DateTime.Now, new List<Answer>(){ new Answer("Jos",5),new Answer("Piet",8),new Answer("Jan",150),new Answer("Nathan",500) }),
-                    new Strawpoll("Wat eet ik het liefst?",DateTime.Now, new List<Answer>(){ new Answer("Pizza",5),new Answer("Burgers",8),new Answer("Spaghetti",150),new Answer("Frieten",500) }),
-                    new Strawpoll("Op welke school zit ik?",DateTime.Now, new List<Answer>(){ new Answer("Sivibu",5),new Answer("VHTI",8),new Answer("Uni Gent",150),new Answer("Hogent",500) }),
-                    new Strawpoll("Wat is mijn favoriete kleur?",DateTime.Now, new List<Answer>(){ new Answer("Geel",5),new Answer("Groen",8),new Answer("Rood",150),new Answer("Blauw",500) }, new List<VotedUUID>(){new VotedUUID("test") }),
-                    new Strawpoll("Dit is een zeer lange vraag maar kun je ze lezen?",DateTime.Now, new List<Answer>(){ new Answer("Ja",5),new Answer("Nee",980) }),
-                    new Strawpoll("Heeft deze vraag een zeer lang antwoord?",DateTime.Now, new List<Answer>(){ new Answer("Ja, deze vraag heeft het langste antwoord van alle vragen",50000),new Answer("Nee",980) })
+                    new Strawpoll("How many children has Queen Elizabeth the Second got?",DateTime.Now, new List<Answer>(){ new Answer("1",20),new Answer("2",10),  new Answer("3",12), new Answer("4",9), new Answer("5",15), new Answer("6",30)}),
+                    new Strawpoll("What colour is a panda?",DateTime.Now, new List<Answer>(){ new Answer("Yellow and white",9),new Answer("Pink and white",12),new Answer("Blue and white",15),new Answer("Gold and blue",23),new Answer("Black and white",48) }),
+                    new Strawpoll("What language has the most words?",DateTime.Now, new List<Answer>(){ new Answer("Dutch",5),new Answer("French",8),new Answer("German",150),new Answer("Chinese",500), new Answer("English", 25) }),
+                    new Strawpoll("What temperature does water boil at?",DateTime.Now, new List<Answer>(){ new Answer("50",0),new Answer("100",150),new Answer("30",0),new Answer("20",0), new Answer("180",20) }),
+                    new Strawpoll("Where was Lope De Vega born?",DateTime.Now, new List<Answer>(){ new Answer("Rome",50),new Answer("Amsterdam",8),new Answer("Paris",150),new Answer("Berlin",240), new Answer("Madrid",800) }),
+                    new Strawpoll("When did the first World War start?",DateTime.Now, new List<Answer>(){ new Answer("1914",50),new Answer("1915",10),new Answer("1917",5),new Answer("1916",18) }),
+                    new Strawpoll("What's the most important book in the Islam?",DateTime.Now, new List<Answer>(){ new Answer("Bible",500),new Answer("Torah",800),new Answer("Lord Of The Rings",0),new Answer("Harry Potter",200), new Answer("Koran",8551) }),
+                    new Strawpoll("What's the capital of Honduras",DateTime.Now, new List<Answer>(){ new Answer("Tegucigalpa",5),new Answer("Paris",8),new Answer("Sydney",150),new Answer("Rome",500), new Answer("Madrid",8) }),
+                    new Strawpoll("What did the crocodile swallow in 'Peter Pan'?",DateTime.Now, new List<Answer>(){ new Answer("An alarm clock",1705),new Answer("Tinkerbell",520) , new Answer("Captain Hook's hook",355), new Answer("A pen",237),new Answer("A gauntlet",94)}),
+                    new Strawpoll("What's the best known artificial international language?",DateTime.Now, new List<Answer>(){ new Answer("Dutch",200),new Answer("Esperanto",980) })
                 };
 
                 strawpolls.ForEach(e => _ctx.Strawpolls.Add(e));
